1251b26e77ab3b05b886c5fda61a4021087eb063
lizagibradze <oreogibradze@gmail.com>
2025-08-04 23:02:39 +0400

    commit

---
+++ day96/classwork/src/App.jsx
@@ -0,0 +1,19 @@
+import ButtonUsage from "./components/ui/button";
+
+export default function App(){
+  return(
+    <div>
+      <ButtonUsage>hiii</ButtonUsage>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
---
+++ day96/classwork/src/components/ui/button.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function ButtonUsage({children}) {
+  return <Button variant="contained">{children}</Button>;
+}
+
+
+
