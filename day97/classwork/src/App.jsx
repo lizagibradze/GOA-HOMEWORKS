f4f191249d0f6ae06143931a4ad69abbe41896e2
lizagibradze <oreogibradze@gmail.com>
2025-08-11 08:19:46 +0400

    commit

---
+++ day97/classwork/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EnhancedTable from "../components/ui/EnhancedTable"
+
+export default function App() {
+  return (
+    <div>
+      <Enhanced-table></Enhanced-table>
+    </div>
+  )
+}
+
+
